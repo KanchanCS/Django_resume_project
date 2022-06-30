dae0902dbc1ad08e9261f5299b998b5e22997493
KanchanCS <imkanchan7422@gmail.com>
2022-06-30 21:15:20 +0530

    service and skill page convert static to dyanmic create Model Skill and Service y

--- education/views.py
+++ education/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from .models import Skill
 # Create your views here.
 def skill(request):
-    context={'skill': 'active'}
-    return render(request, 'education/skill.html', context)
+    context = Skill.objects.all()
+    return render(request, 'education/skill.html', {"context":context})
